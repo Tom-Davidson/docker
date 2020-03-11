50a32effbff4e90ad8bdb72080c09f79031e3569
Anthony Sargeant <anthony.sargeant@sky.uk>
2020-03-11 14:57:18 +0000

    [CIE-628] Build artefact now passed in as a build argument

--- simplerisk-ubuntu1804/simplerisk/start.sh
+++ simplerisk-ubuntu1804/simplerisk/start.sh
@@ -7,7 +7,12 @@ mkdir -p /etc/apache2/ssl/ssl.crt
 echo ${CERTIFICATE} | sed 's/\\n/\n/g' > /etc/apache2/ssl/ssl.crt/simplerisk.crt
 
 # Update the SimpleRisk config file from the ENV vars
-cat /var/www/config.orig.php | sed "s/DB_HOSTNAME', 'localhost/DB_HOSTNAME', '$MYSQL_HOSTNAME/" | sed "s/DB_PASSWORD', 'simplerisk/DB_PASSWORD', '$MYSQL_PASSWORD/" | sed "s/DB_USERNAME', 'simplerisk/DB_USERNAME', '$MYSQL_USER/" > /var/www/simplerisk/includes/config.php
+cat /var/www/config.orig.php |\
+ sed "s/DB_HOSTNAME', 'localhost/DB_HOSTNAME', '${MYSQL_HOSTNAME}/" |\
+ sed "s/DB_PASSWORD', 'simplerisk/DB_PASSWORD', '${MYSQL_PASSWORD}/" |\
+ sed "s/DB_USERNAME', 'simplerisk/DB_USERNAME', '${MYSQL_USER}/" |\
+ sed "s:DB_SSL_CERTIFICATE_PATH', ':DB_SSL_CERTIFICATE_PATH', '${MYSQL_SSL_CERTIFICATE_PATH}:"\
+ > /var/www/simplerisk/includes/config.php
 
 # Start Apache
 /usr/sbin/apache2ctl -D FOREGROUND

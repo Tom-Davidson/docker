2d7a83212ffcfefe586ed4c5a163da94de6edb36
Anthony Sargeant <anthony.sargeant@sky.uk>
2020-03-14 18:18:26 +0000

    [CIE-628] WIP: Fixed code review comments, so SSL is now optional. Now re-enabling the localhost deployment via docker-compose-local.yml which is currently not working (hence this being a WIP commit)

--- simplerisk-ubuntu1804/simplerisk/start.sh
+++ simplerisk-ubuntu1804/simplerisk/start.sh
@@ -7,11 +7,11 @@ mkdir -p /etc/apache2/ssl/ssl.crt
 echo "${CERTIFICATE}" | base64 -d | sed 's/\\n/\n/g' > /etc/apache2/ssl/ssl.crt/simplerisk.crt
 
 # Update the SimpleRisk config file from the ENV vars
-cat /var/www/config.orig.php |\
- sed "s/DB_HOSTNAME', 'localhost/DB_HOSTNAME', '${MYSQL_HOSTNAME}/" |\
- sed "s/DB_PASSWORD', 'simplerisk/DB_PASSWORD', '${MYSQL_PASSWORD}/" |\
- sed "s/DB_USERNAME', 'simplerisk/DB_USERNAME', '${MYSQL_USER}/" |\
- sed "s:DB_SSL_CERTIFICATE_PATH', ':DB_SSL_CERTIFICATE_PATH', '${MYSQL_SSL_CERTIFICATE_PATH}:"\
+cat /var/www/config.orig.php | \
+ sed "s/DB_HOSTNAME', 'localhost/DB_HOSTNAME', '${MYSQL_HOSTNAME}/" | \
+ sed "s/DB_PASSWORD', 'simplerisk/DB_PASSWORD', '${MYSQL_PASSWORD}/" | \
+ sed "s/DB_USERNAME', 'simplerisk/DB_USERNAME', '${MYSQL_USER}/" | \
+ sed "s:DB_SSL_CERTIFICATE_PATH', ':DB_SSL_CERTIFICATE_PATH', '${MYSQL_SSL_CERTIFICATE_PATH:-}:" \
  > /var/www/simplerisk/includes/config.php
 
 # Start Apache
